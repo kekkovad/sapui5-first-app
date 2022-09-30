21eac905ac8dce3c6279f7333fb6007e9745ad17
Matteo Convertino <mconvertino@deloitte.it>
2022-09-30 07:59:41 +0000

    init template ui5

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["btp/academy/firstfioriapp/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"btpacademy/first-fiori-app/test/unit/controller/Main.controller"
+], function () {
+	"use strict";
+});
