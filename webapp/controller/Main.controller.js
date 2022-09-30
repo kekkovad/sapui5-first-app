96b7b2479521c91270e74be0fad54d25f13a76c4
Matteo Convertino <mconvertino@deloitte.it>
2022-09-30 08:43:04 +0000

    oEvent

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,18 +1,17 @@
-sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-],
-    /**
-     * @param {typeof sap.ui.core.mvc.Controller} Controller
-     */
-    function (Controller) {
-        "use strict";
+sap.ui.define(
+  ["sap/ui/core/mvc/Controller", "sap/m/MessageToast"],
+  /**
+   * @param {typeof sap.ui.core.mvc.Controller} Controller
+   */
+  function (Controller, MessageToast) {
+    "use strict";
 
-        return Controller.extend("btp.academy.firstfioriapp.controller.Main", {
-            onInit: function () {
-
-            },
-            onPress : function() {
-                console.log("Button pressed")
-            }
-        });
+    return Controller.extend("btp.academy.firstfioriapp.controller.Main", {
+      onInit: function () {},
+      onPress: function (oEvent) {
+        const sId = oEvent.getParameter("id");
+        MessageToast.show("Button pressed: " + sId);
+      },
     });
+  }
+);
